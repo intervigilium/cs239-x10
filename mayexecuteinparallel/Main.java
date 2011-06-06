495957fe6e527af723e3d2d86f4bf903058cda2a
Ethan Chen <intervigil@gmail.com>
2011-06-05 17:43:44 -0700

    build x10 parser correctly using jtb and javacc

--- mayexecuteinparallel/Main.java
+++ mayexecuteinparallel/Main.java
@@ -5,7 +5,7 @@ public class Main {
    public static void main(String [] args) {
       try {
          // new MiniX10Parser(System.in).Goal();
-         new MiniX10Parser(System.in).File();
+         INode root = new MiniX10Parser(System.in).File();
       }
       catch (ParseException e) {
          System.out.println(e.toString());
