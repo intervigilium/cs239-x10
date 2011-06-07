ebb23aaea0cdd6332b381972081a2209b89a9265
Ethan Chen <intervigil@gmail.com>
2011-06-06 18:24:10 -0700

    move may execute in parallel visitor to cs239 namespace, extend from DepthFirstVoidVisitor

--- mayexecuteinparallel/Main.java
+++ mayexecuteinparallel/Main.java
@@ -1,15 +1,15 @@
 import syntaxtree.*;
 import visitor.*;
+import cs239.*;
 
 public class Main {
-   public static void main(String [] args) {
-      try {
-         // new MiniX10Parser(System.in).Goal();
-         INode root = new MiniX10Parser(System.in).File();
-      }
-      catch (ParseException e) {
-         System.out.println(e.toString());
-      }
-   }
+    public static void main(String [] args) {
+        try {
+            INode root = new MiniX10Parser(System.in).File();
+            MayExecuteInParallelVisitor scanner = new MayExecuteInParallelVisitor();
+            root.accept(scanner);
+        } catch (ParseException e) {
+            System.out.println(e.toString());
+        }
+    }
 }
-
